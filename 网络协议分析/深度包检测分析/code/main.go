2a7b67319eaa2e2cce7b8839b09138f8b22198fc
ma5hr00m <108560334+ma5hr00m@users.noreply.github.com>
2023-12-28 21:22:21 -0500

    深度包检测分析验收完成

--- 网络协议分析/深度包检测分析/code/main.go
+++ 网络协议分析/深度包检测分析/code/main.go
@@ -33,19 +33,24 @@ type ServiceTypeStats struct {
 
 // 获取数据包的服务类型
 func getServiceType(packet gopacket.Packet) string {
+	// 检查是否为TCP数据包
 	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 		tcp, _ := tcpLayer.(*layers.TCP)
+		// 检查TCP端口是否为22（SSH）
 		if tcp.SrcPort == layers.TCPPort(22) || tcp.DstPort == layers.TCPPort(22) {
 			return BestEffortDelivery
 		}
+		// 检查TCP端口是否为80（HTTP）、443（HTTPS）
 		if tcp.SrcPort == layers.TCPPort(80) || tcp.DstPort == layers.TCPPort(80) || tcp.SrcPort == layers.TCPPort(443) || tcp.DstPort == layers.TCPPort(443) {
 			return GuaranteedLatency
 		}
 		return GuaranteedDelivery
 	}
 
+	// 检查是否为UDP数据包
 	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
 		udp, _ := udpLayer.(*layers.UDP)
+		// 检查UDP端口是否为5060、5061（SIP）
 		if udp.SrcPort == layers.UDPPort(5060) || udp.DstPort == layers.UDPPort(5060) || udp.SrcPort == layers.UDPPort(5061) || udp.DstPort == layers.UDPPort(5061) {
 			return LowLatency
 		}
@@ -57,16 +62,19 @@ func getServiceType(packet gopacket.Packet) string {
 
 // 更新服务统计信息
 func updateServiceStats(stats map[string]*ServiceStats, packet gopacket.Packet, serviceType string) {
+	// 检查服务类型是否存在于统计信息中，如果不存在则创建一个新的统计信息
 	if _, ok := stats[serviceType]; !ok {
 		stats[serviceType] = &ServiceStats{}
 	}
 	length := packet.Metadata().CaptureLength
 	hosts := make(map[string]bool)
+	// 获取IPv4层的源IP和目标IP，并将其添加到hosts映射中
 	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv4)
 		hosts[ip.SrcIP.String()] = true
 		hosts[ip.DstIP.String()] = true
 	}
+	// 更新统计信息中的主机数量和数据包比例
 	stats[serviceType].HostCount += len(hosts)
 	stats[serviceType].PacketRatio += float64(length)
 	packets := []struct {
@@ -78,6 +86,7 @@ func updateServiceStats(stats map[string]*ServiceStats, packet gopacket.Packet, 
 			Timestamp: packet.Metadata().Timestamp,
 		},
 	}
+	// 计算平均速度并更新统计信息中的平均速度
 	stats[serviceType].AverageSpeed = calcAvgSpeed(packets)
 }
 
@@ -86,16 +95,20 @@ func calcAvgSpeed(packets []struct {
 	Length    int
 	Timestamp time.Time
 }) float64 {
+	// 如果数据包数量小于2，则返回0.0
 	if len(packets) < 2 {
 		return 0.0
 	}
 	firstPacketTime := packets[0].Timestamp
 	lastPacketTime := packets[len(packets)-1].Timestamp
 	totalBytes := 0
+	// 计算所有数据包的总字节数
 	for _, packet := range packets {
 		totalBytes += packet.Length
 	}
+	// 计算时间间隔（秒）
 	elapsedTime := lastPacketTime.Sub(firstPacketTime).Seconds()
+	// 计算平均速度（字节/秒）
 	return float64(totalBytes) / elapsedTime
 }
 
@@ -111,6 +124,7 @@ func plotServiceStats(serviceStats []*ServiceTypeStats) {
 	packetRatios := make(plotter.Values, len(serviceStats))
 	averageSpeeds := make(plotter.Values, len(serviceStats))
 
+	// 从服务统计信息中提取数据
 	for i, stat := range serviceStats {
 		serviceTypes[i] = stat.ServiceType
 		hostCounts[i] = float64(stat.Stats.HostCount)
@@ -118,6 +132,7 @@ func plotServiceStats(serviceStats []*ServiceTypeStats) {
 		averageSpeeds[i] = stat.Stats.AverageSpeed
 	}
 
+	// 创建柱状图
 	hostCountBar, err := plotter.NewBarChart(hostCounts, vg.Points(20))
 	if err != nil {
 		log.Fatal(err)
@@ -141,12 +156,14 @@ func plotServiceStats(serviceStats []*ServiceTypeStats) {
 	averageSpeedBar.Color = plotutil.Color(2)
 	averageSpeedBar.Offset = vg.Points(10)
 
+	// 将柱状图添加到图表中并设置图例
 	p.Add(hostCountBar, packetRatioBar, averageSpeedBar)
 	p.Legend.Add("主机数量", hostCountBar)
 	p.Legend.Add("数据包比例", packetRatioBar)
 	p.Legend.Add("平均速度", averageSpeedBar)
 	p.NominalX(serviceTypes...)
 
+	// 保存图表为PNG图片
 	if err := p.Save(6*vg.Inch, 4*vg.Inch, "service_stats.png"); err != nil {
 		log.Fatal(err)
 	}
@@ -159,26 +176,31 @@ func main() {
 	}
 	defer handle.Close()
 
+	// 创建数据包源，使用与处理句柄相同的链路类型
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	serviceStats := make(map[string]*ServiceStats) // 服务统计信息
+	serviceStats := make(map[string]*ServiceStats)
 
 	totalPackets := 0
 
 	for packet := range packetSource.Packets() {
 		totalPackets++
+
 		serviceType := getServiceType(packet)
+
 		updateServiceStats(serviceStats, packet, serviceType)
-		if totalPackets == 3600 {
+
+		if totalPackets == 7200 {
 			break
 		}
 	}
 
+	// 归一化处理服务统计信息
 	for _, stat := range serviceStats {
 		stat.PacketRatio = stat.PacketRatio / float64(totalPackets)
 	}
 
-	// 将 serviceStats 转换为 []*ServiceTypeStats 类型
+	// 将服务统计信息映射表转换为切片类型
 	convertedServiceStats := make([]*ServiceTypeStats, 0, len(serviceStats))
 	for serviceType, stats := range serviceStats {
 		convertedServiceStats = append(convertedServiceStats, &ServiceTypeStats{
@@ -187,5 +209,6 @@ func main() {
 		})
 	}
 
+	// 绘制服务统计图表并保存为文件
 	plotServiceStats(convertedServiceStats)
 }
--- 网络协议分析/深度包检测分析/code/utils/capture.py
+++ 网络协议分析/深度包检测分析/code/utils/capture.py
@@ -5,6 +5,6 @@ def packet_callback(packet):
 
 print("[事件]：开始捕获数据包")
 
-sniff(iface='lo', timeout=10, prn=packet_callback)
+sniff(iface='lo', timeout=1800, prn=packet_callback)
 
 print("[事件]：数据包捕获完成并保存为package.pcap")
